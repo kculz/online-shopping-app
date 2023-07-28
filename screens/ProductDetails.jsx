c0afa2ffd81a3dce7f3588146c0127f7a6d182d4
kculz <munyamakudzai095@gmail.com>
2023-07-28 22:35:32 +0200

    Error: productDetails page

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,15 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { COLORS, SIZES } from '../constants'
+import { Ionicons } from '@expo/vector-icons'
 
-const ProductDetails = () => {
+const ProductDetails = ({navigation}) => {
   return (
-    <SafeAreaView>
-      <Text>ProductDetails</Text>
-    </SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.upperRow}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={{flex:1}} >
+            <Ionicons name='chevron-back-circle' size={30} />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Ionicons name='heart' size={30} />
+          </TouchableOpacity>
+        </View>
+      </View>
   )
 }
 
 export default ProductDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  },
+  upperRow: {
+    marginHorizontal:20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute",
+    top: SIZES.xxl,
+    // width: SIZES.xxl,
+    zIndex:999
+  }
+})
